e570510870ca860c9213935fc925b8dc1ac5ed36
Abel Dessalegne <abelkelemu60@gmail.com>
2022-07-20 20:28:51 +0300

    a

--- src/components/StudentsArea/index.js
+++ src/components/StudentsArea/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import {CKEditor}  from 'ckeditor4-react';
 import './styles.scss'
 
 //assets
@@ -15,7 +14,7 @@ import ProgressBar from '../ProgressBar';
 
 //actions
 import { updateImageStart, updateTextStart, fetchUserStart } from '../../redux/User/user.actions';
-import { addImageStart } from '../../redux/Images/images.actions';
+
 
 
 
@@ -28,38 +27,67 @@ const mapState = ({user}) => ({
 const StudentsArea = props => {
 
     const dispatch = useDispatch();
-    const {currentUser, student, userPercentage } = useSelector(mapState);
     const inputRef = React.useRef();
+    const {currentUser, student, userPercentage } = useSelector(mapState);
+    
     const {id } = currentUser;
-    const { yearbookimgOneThumbnail} = student;
+    const { yearbookimgOneThumbnail, yellowpageimgThumbnail,firstName,lastName,phoneNumber,emailAddress,instagramUsername,facebookUsername,yearbookQuote} = student;
 
     const [hideModal, setHideModal] = useState(true);
+    const [hideModal2, setHideModal2] = useState(true);
+
     const [image, setImage] = useState(null);
     const [imagePreview, setimagePreview] = useState();
+
     const [error,setError] = useState('');
+
+    const [fN,setFN] = useState(firstName);
+    const [lN,setLN] = useState(lastName);
+    const[ybQ, setYbQ] = useState(yearbookQuote);
    
-   // const [tempoID, setTempoID] = useState('');
-    
+   
+    useEffect(() => {
+        dispatch(
+            fetchUserStart(id)
+        )
+    },[])
+
     const resetImage = () => setImage(null);
     
     const triggerFileSelectPopup = (e) => {
         e.preventDefault()
         inputRef.current.click();
-      }
+    }
 
-    const toggleModal = () => setHideModal(!hideModal);
-   
+    const toggleModal = () => {
+      setHideModal(!hideModal)
+      setimagePreview()
+      setImage(null)
+    };
+      
+    const toggleModal2 = () => {
+      setHideModal2(!hideModal2)
+      setFN(firstName);
+      setLN(lastName);
+      setYbQ(yearbookQuote);
+    }
+    
     const resetForm = () =>{
       setHideModal(true);
-    // setTempoID()
       setimagePreview()
       setImage(null)
       
     }
+
     const configModal = {
-        hideModal,
-        toggleModal,
+        hideModal:hideModal,
+        toggleModal:toggleModal,
     };
+
+    const configModal2 = {
+      hideModal: hideModal2,
+      toggleModal : toggleModal2,
+  };
        
     const handleSubmit = e => {
         e.preventDefault();
@@ -85,21 +113,37 @@ const StudentsArea = props => {
         )
     }
 
-      // const handleSubmit3 = e => {
-      //   e.preventDefault();
-      //   dispatch(
-      //     addImageStart({image})  
-      //       )
-      //    resetForm();
-      // }
+    const handleSubmit3 = e => {
+      e.preventDefault();
+      dispatch(
+        updateTextStart({
+          id ,
+          field :'firstName',
+          newText: fN
+        })  
+      )
+      dispatch(
+        updateTextStart({
+          id ,
+          field :'lastName',
+          newText: lN
+        })  
+      )
+      dispatch(
+        updateTextStart({
+          id ,
+          field :'yearbookQuote',
+          newText: ybQ
+        })  
+      )
+      setHideModal2(true);
+  }
 
     const types = ['image/png', 'image/jpeg', 'image/jpg']; 
     
     const ImgHandler = (e) => {
 
-        let selectedFile = e.target.files[0];
-       // let selectedFileSize = e.target.files[0].size;
-      
+        let selectedFile = e.target.files[0];     
         if (selectedFile && types.includes(selectedFile.type)) {        
             setImage(selectedFile);
             setError('')
@@ -110,30 +154,33 @@ const StudentsArea = props => {
         }
     }
     
-    
-    useEffect(() => {
-
-        dispatch(
-            fetchUserStart(id)
-        )
-    },[yearbookimgOneThumbnail])
-
-
     return (
  
-        <div className='accountWrap'>
-         
-            <h1>Yearbook</h1>          
+        <div className='StudentAreaWrap'>
+
+          <div className="yearbookImgWrap">
+
+            <h1>Yearbook</h1>  
             {userPercentage>0 && <ProgressBar percentage = {userPercentage}/>}
             {yearbookimgOneThumbnail? <img src={yearbookimgOneThumbnail} alt="" /> : <img src ={uploadImg} onClick={() => toggleModal()} />}
-     
+           
+            <div className="icons">
+              <div className="deleteIcon">
+                {userPercentage==0 && <i title="Delete Picture" className = "deleteIcon"class="fa fa-trash-alt" aria-hidden="true" onClick={handleSubmit2}></i>}
+              </div>
+              <div className="editIcon">
+                {userPercentage == 0 && <i title = "Edit Picture" class="fa fa-edit" aria-hidden="true" onClick={() => toggleModal()}></i>}
+              </div>
+            </div>
+
+            
             <Modal {...configModal}>
 
-              <div className="updateProductImage">
+              <div className="updateYearbookImg">
               
                 <form onSubmit = {handleSubmit}>                
               
-                  <label htmlFor="yearbook-small-image"><h2>Image One</h2></label>
+                  <label htmlFor="yearbookImage"><h2>Yearbook Photo</h2></label>
               
                     <Button onClick={triggerFileSelectPopup}>
                      Choose an image
@@ -141,8 +188,8 @@ const StudentsArea = props => {
               
                     <input 
                       type="file" 
-                      name="yearbook-small-image"
-                      className="imgoneinput" 
+                      name="yearbookImage"
+                      className="yearbookImgInput" 
                       onChange={ImgHandler} 
                       ref ={inputRef} 
                       style={{display:"none"}}
@@ -169,11 +216,65 @@ const StudentsArea = props => {
                 </form>
               </div>
             </Modal>
+            
+          </div>
+          
+          <div className="yearbookInfo">
+              <ul>
+                <li>
+                  First Name: {firstName && firstName}
+                  
+                </li>
+                <li>
+                  Last Name: {lastName && lastName}
+                  
+                </li>
+                <li>
+                  Yearbook Quote: "{yearbookQuote && yearbookQuote}"
+                  
+                </li>
+              </ul>
+              
+              <div className="iconTextEdit">
+                   <i title="Edit First Name "class="fa fa-edit" aria-hidden="true" onClick={() => toggleModal2()}></i>
+              </div>
+              <Modal {...configModal2}>
+                  <form onSubmit={handleSubmit3}>
+                    First Name
+                    <FormInput
+                      type= "text"
+                      name="fN"
+                      value={fN}
+                      placeholder={fN}
+                      handleChange = {e => setFN(e.target.value)}
+                      />
+                      Last Name
+                      <FormInput
+                      type= "text"
+                      name="lN"
+                      value={lN}
+                      placeholder={lN}
+                      handleChange = {e => setLN(e.target.value)}
+                      />
+                      Yearbook Quote
+                      <FormInput
+                      type= "text"
+                      name="ybQ"
+                      value={ybQ}
+                      placeholder={ybQ}
+                      handleChange = {e => setYbQ(e.target.value)}
+                      />
+                      <Button type= "submit">
+                        Submit
+                      </Button>
+                  </form>
+              </Modal>
 
-            <i class="fa fa-trash-alt" aria-hidden="true" onClick={handleSubmit2}></i>
-            <i class="fa fa-edit" aria-hidden="true" onClick={() => toggleModal()}></i>
-                
+          </div>
+           
             <h1>Yellow Page</h1>
+
+            
         </div>
 
   
